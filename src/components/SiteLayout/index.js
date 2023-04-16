e449fadc2e203c5953fb990c68e56e6d3eaad91c
Bijay Sharma <bijay.sharma@broadcom.com>
2023-04-16 23:54:18 +0530

    Add Local Logo

--- src/components/SiteLayout/index.js
+++ src/components/SiteLayout/index.js
@@ -1,6 +1,7 @@
 import { Layout } from 'antd';
 import './index.css';
 import { Navbar,Text } from "@nextui-org/react";
+import sistLogo from './logo.jpg'
 
 const { Footer } = Layout;
 
@@ -13,7 +14,7 @@ const SiteLayout = ({ children }) => {
           <img style={{
             width: "35px",
             height: "35px",
-          }} src="https://scontent.frdp1-1.fna.fbcdn.net/v/t1.6435-9/59440341_453576372045077_104671922121342976_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=4bdSXXYBTUkAX9hnMCy&_nc_ht=scontent.frdp1-1.fna&oh=00_AfB_UfiCmaqsjfgx-KS5kHETygEEjD2VBpC_Ae2kxFvQhg&oe=646353C4" alt="logo" />
+          }} src={sistLogo} alt="logo" />
           <Text b color="inherit" style={{
             fontFamily: "helvetica",
             marginLeft: "10px",
